1483288bd984671e106bdf60e1ccf1a54b59316c
Darek Bittner <33453332+highpeaks@users.noreply.github.com>
2018-10-02 14:17:15 -0400

    bad habits

--- public/sketch.js
+++ public/sketch.js
@@ -62,7 +62,7 @@ function refresh(){
 		for (let i = 0; i < data.tweets.length; i++){
 			paragraphs[i].html(data.tweets[i]);
 		}
-		for (let j = data.tweets.length; j < 50; j++){
+		for (let j = data.tweets.length; j < 10; j++){
 			paragraphs[j].html('');
 		}
 
